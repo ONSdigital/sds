bb8e92cd1976ac41c32a9792557006f8ddd2603d
James Smith <james.smith@methods.co.uk>
2023-02-02 13:19:20 +0000

    A trivial change.

--- src/scratch.py
+++ src/scratch.py
@@ -1,4 +1,5 @@
 from database import get_data, set_data
+from constants import UNIT_ID
 
 
 dataset_id = "1"
@@ -6,7 +7,7 @@ dataset_id = "1"
 unit_id = "2"
 
 data = {
-    "unit_id": unit_id
+    UNIT_ID: unit_id
 }
 
 set_data(dataset_id, data)
