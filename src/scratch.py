38b5aa0ed674b907c73f678eb1176c4d970038dd
James Smith <james.smith@methods.co.uk>
2023-02-02 12:36:44 +0000

    Gotten the connectivity working.

--- src/scratch.py
+++ src/scratch.py
@@ -12,7 +12,7 @@ credentials = firebase_admin.credentials.Certificate(FIREBASE_KEYFILE_LOCATION)
 projectId = FIRESTORE_PROJECT_ID
 
 options = {
-    'projectId': projectId
+    "projectId": projectId
 }
 
 firebase_admin.initialize_app(credentials, options)
@@ -20,4 +20,29 @@ firebase_admin.initialize_app(credentials, options)
 client = firestore.client()
 
 
-print("ok!")
+datasets_collection = client.collection("datasets")
+
+schemas_collection = client.collection("schemas")
+
+
+def set_data(dataset_id, data):
+    units_collection = datasets_collection.document(dataset_id).collection("units")
+    units_collection.document(data["unit_id"]).set(data)
+
+
+def get_data(dataset_id, unit_id):
+    units_collection = datasets_collection.document(dataset_id).collection("units")
+    return units_collection.document(unit_id).get().to_dict()
+
+
+dataset_id = "1"
+unit_id = "2"
+data = {
+    "unit_id": unit_id
+}
+
+set_data(dataset_id, data)
+
+data = get_data(dataset_id, unit_id)
+
+print(data)
