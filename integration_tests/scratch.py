7d2362eecc3996763d7793f7a05caa4057c1e46d
Daniel Ball <mail@danielball.com>
2023-02-03 12:45:02 +0000

    SDSS-38 linting in cloudbuild (#6)
    Do linting and unit tests in cloud build

--- integration_tests/scratch.py
+++ integration_tests/scratch.py
@@ -1,8 +1,7 @@
 import os
+
 import firebase_admin
-
 from firebase_admin import firestore
-
 
 FIRESTORE_PROJECT_ID = os.environ.get("FIRESTORE_PROJECT_ID")
 FIREBASE_KEYFILE_LOCATION = os.environ.get("FIREBASE_KEYFILE_LOCATION")
@@ -11,9 +10,7 @@ credentials = firebase_admin.credentials.Certificate(FIREBASE_KEYFILE_LOCATION)
 
 projectId = FIRESTORE_PROJECT_ID
 
-options = {
-    'projectId': projectId
-}
+options = {"projectId": projectId}
 
 firebase_admin.initialize_app(credentials, options)
 
