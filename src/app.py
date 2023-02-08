d83fbbf16351b23dacadca72319c7aec8241c99c
Daniel Ball <mail@danielball.com>
2023-02-08 17:30:18 +0000

    Add Pydantic model

--- src/app.py
+++ src/app.py
@@ -2,11 +2,11 @@ import logging
 import uuid
 
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 import database
 
 logging.basicConfig(level=logging.INFO)
-
 
 app = FastAPI()
 
@@ -45,11 +45,31 @@ async def retrieve_schema(dataset_schema_id: str, version: int):
     return data
 
 
-@app.get("/dataset_schemas")
-async def query_schemas(survey_id: str):
+class SchemaMetadata(BaseModel):
+    survey_id: str
+    schema_location: str
+    sds_schema_version: int
+    sds_published_at: str
+
+
+class Schemas(BaseModel):
+    supplementary_dataset_schema: dict[str, SchemaMetadata]
+
+
+@app.get("/v1/schema_metadata")
+async def query_schemas(survey_id: str) -> Schemas:
     """Retrieve the schemas, given the survey_id."""
-    data = database.get_schemas(survey_id)
-    return data
+    # data = database.get_schemas(survey_id)
+    return Schemas(
+        supplementary_dataset_schema={
+            "x": SchemaMetadata(
+                survey_id="x",
+                schema_location="y",
+                sds_schema_version=1,
+                sds_published_at="x",
+            )
+        }
+    )
 
 
 @app.get("/datasets")
