c686335a9d0f33596da6b5108275e38be5cd8175
Daniel Ball <mail@danielball.com>
2023-03-02 11:18:15 +0000

    Fix comments from code review

--- src/app/main.py
+++ src/app/main.py
@@ -3,29 +3,21 @@ import dataset_storage
 import functions_framework
 
 
-# Triggered by a change in a storage bucket
+#
 @functions_framework.cloud_event
 def new_dataset(cloud_event):
+    """
+    Triggered by uploading a new dataset file to the
+    dataset storage bucket. See the 'Cloud Functions' section
+    in the README.md file for details as to how this function
+    is set up.
+    """
     data = cloud_event.data
-
-    event_id = cloud_event["id"]
-    event_type = cloud_event["type"]
 
     bucket_name = data["bucket"]
     filename = data["name"]
-    metageneration = data["metageneration"]
-    time_created = data["timeCreated"]
-    updated = data["updated"]
 
-    print(f"Event ID: {event_id}")
-    print(f"Event type: {event_type}")
-    print(f"Bucket: {bucket_name}")
-    print(f"File: {filename}")
-    print(f"Metageneration: {metageneration}")
-    print(f"Created: {time_created}")
-    print(f"Updated: {updated}")
     dataset_id = filename.split(".json")[0]
-    print(f"dataset_id: {dataset_id}")
 
     dataset = dataset_storage.get_dataset(filename=filename, bucket_name=bucket_name)
     database.set_dataset(dataset_id=dataset_id, dataset=dataset)
