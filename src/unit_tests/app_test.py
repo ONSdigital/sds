bfe117f754a99160b18654f6079e85ec52449966
Daniel Ball <mail@danielball.com>
2023-03-17 10:20:16 +0000

    Fix unit tests

--- src/unit_tests/app_test.py
+++ src/unit_tests/app_test.py
@@ -103,6 +103,7 @@ def test_get_dataset_metadata(client, database):
         "period_id": "abc",
         "title": "Which side was better?",
         "sds_schema_version": 4,
+        "sds_dataset_version": 23,
         "sds_published_at": "2023-03-13T14:34:57Z",
         "total_reporting_units": 2,
         "schema_version": "v1.0.0",
