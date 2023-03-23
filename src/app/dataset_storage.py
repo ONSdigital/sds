79fa3b6a5f622ac05dc8a370acb1fb345963ab3d
Mike Tidman <mike.tidman@ons.gov.uk>
2023-03-23 08:44:54 +0000

    PR changes

--- src/app/dataset_storage.py
+++ src/app/dataset_storage.py
@@ -5,11 +5,13 @@ from google.cloud import storage
 
 storage_client = storage.Client()
 
-# If the local development is used then, it will use those values to create the local storage.
-if os.environ.get("DATASET_BUCKET_NAME"):
-    bucket = storage_client.bucket(os.environ.get("DATASET_BUCKET_NAME"))
+
+bucket_name = os.environ.get("DATASET_BUCKET_NAME")
+
+if bucket_name:
+    bucket = storage_client.bucket(bucket_name)
     if not bucket.exists():
-        bucket = storage_client.create_bucket(os.environ.get("DATASET_BUCKET_NAME"))
+        bucket = storage_client.create_bucket(bucket_name)
 elif os.environ.get("STORAGE_EMULATOR_HOST"):
     bucket = storage_client.create_bucket("dataset_bucket")
 
