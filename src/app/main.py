f13e97b895edf5279deaf1106eb08c047b99a430
Jack Rimmer <jack.rimmer@and.digital>
2023-04-12 13:01:27 +0100

    Cloud function success log

--- src/app/main.py
+++ src/app/main.py
@@ -2,7 +2,10 @@ import uuid
 
 import database
 import dataset_storage
+from logging_config import logging
 import functions_framework
+
+logger = logging.getLogger(__name__)
 
 
 @functions_framework.cloud_event
@@ -14,6 +17,8 @@ def new_dataset(cloud_event):
     is set up.
     * The dataset_id is an auto generated GUID and the filename is saved as a new field in the metadata.
     """
+    logger.info('Uploading new dataset...')
+
     data = cloud_event.data
 
     bucket_name = data["bucket"]
@@ -23,3 +28,5 @@ def new_dataset(cloud_event):
 
     dataset = dataset_storage.get_dataset(filename=filename, bucket_name=bucket_name)
     database.set_dataset(dataset_id=dataset_id, filename=filename, dataset=dataset)
+    logger.info('Dataset successfully uploaded.')
+
---
+++ src/unit_tests/cloud_new_dataset/new_dataset_logs_test.py
@@ -0,0 +1,35 @@
+from unittest.mock import patch, MagicMock
+import logging
+
+
+@patch('dataset_storage.get_dataset')
+@patch('database.set_dataset')
+def test_new_dataset(
+        dataset_storage_mock,
+        set_dataset_mock,
+        caplog,
+        cloud_functions,
+    ):
+    """
+    There should be a log for when the cloud function is triggered and when the
+    new dataset is successfully uploaded.
+    """
+    caplog.set_level(logging.INFO)
+    dataset_storage_mock.return_value = {}
+    set_dataset_mock.return_value = {}
+    cloud_event = MagicMock()
+    cloud_event.data = {
+        "id": "test_id",
+        "type": "test_type",
+        "bucket": "test_bucket",
+        "metageneration": "1",
+        "timeCreated": "test_time_created",
+        "updated": "test_time_updated",
+        "name": "test_name.json",
+    }
+
+    cloud_functions.new_dataset(cloud_event=cloud_event)
+
+    assert len(caplog.records) == 2
+    assert caplog.records[0].message == "Uploading new dataset..."
+    assert caplog.records[1].message == "Dataset successfully uploaded."
