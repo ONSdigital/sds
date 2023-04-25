a617fb24f6a52ad64c6896fe935efe01305ad44c
Jack Rimmer <jack.rimmer@and.digital>
2023-04-25 14:09:38 +0100

    Test new commit

--- src/app/services/datetime_service.py
+++ src/app/services/datetime_service.py
@@ -5,6 +5,6 @@ class DatetimeService:
     @staticmethod
     def get_current_date_and_time() -> datetime:
         """
-        Gets current date and time. Wrapper for datetime.now object to make it easier to mock for tests.
+        Gets current date and time. Wrapper for datetime.now() object to make it easier to mock for tests.
         """
         return datetime.now()
