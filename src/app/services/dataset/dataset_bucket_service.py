9fb20d0bf9511176978ec2c78d1b2783b2ccff85
Jack Rimmer <jack.rimmer@and.digital>
2023-05-12 16:38:25 +0100

    Unit test refactor

--- src/app/services/dataset/dataset_bucket_service.py
+++ src/app/services/dataset/dataset_bucket_service.py
@@ -1,10 +1,14 @@
+from logging_config import logging
 from models.dataset_models import UnitDataset
 from repositories.buckets.dataset_bucket_repository import DatasetBucketRepository
 from services.validators.dataset_validator_service import DatasetValidatorService
 
+logger = logging.getLogger(__name__)
+
 
 class DatasetBucketService:
     def __init__(self, bucket_name):
+        self.bucket_name = bucket_name
         self.dataset_bucket_repository = DatasetBucketRepository(bucket_name)
 
     def get_dataset_with_cleanup(self, filename: str) -> UnitDataset:
@@ -13,8 +17,15 @@ class DatasetBucketService:
         raw_dataset_with_metadata = (
             self.dataset_bucket_repository.get_dataset_file_as_json(filename)
         )
-        self.dataset_bucket_repository.empty_bucket()
+        self._try_empty_bucket()
 
         DatasetValidatorService.validate_raw_dataset(raw_dataset_with_metadata)
 
         return raw_dataset_with_metadata
+
+    def _try_empty_bucket(self) -> None:
+        try:
+            self.dataset_bucket_repository.empty_bucket()
+        except Exception as e:
+            logger.debug(f"Failed to empty bucket {self.bucket_name} with error: {e}")
+            raise RuntimeError("Failed to empty dataset bucket.")
--- src/unit_tests/test_helper.py
+++ src/unit_tests/test_helper.py
@@ -1,10 +1,38 @@
+import json
 from typing import Generator
 from unittest.mock import MagicMock
 
+from config.config_factory import ConfigFactory
 from google.cloud.firestore_v1.document import DocumentSnapshot
+from models.dataset_models import UnitDataset
+from repositories.buckets.dataset_bucket_repository import DatasetBucketRepository
+
+config = ConfigFactory.get_config()
 
 
 class TestHelper:
+    @staticmethod
+    def new_dataset_mock(cloud_event):
+        """
+        Mocks the cloud function call.
+        """
+        from main import new_dataset
+
+        return new_dataset(cloud_event)
+
+    @staticmethod
+    def mock_get_dataset_from_bucket():
+        """
+        Mocks the application's google bucket boundaries.
+        """
+        with open(config.TEST_DATASET_PATH) as f:
+            dataset_with_metadata: UnitDataset = json.load(f)
+
+        DatasetBucketRepository.get_dataset_file_as_json = MagicMock()
+        DatasetBucketRepository.get_dataset_file_as_json.return_value = (
+            dataset_with_metadata
+        )
+
     @staticmethod
     def create_document_snapshot_generator_mock(
         yield_data_collection,
