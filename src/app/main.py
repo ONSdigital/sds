f7aaa2c0779d68a83187fd8f794a234e37317ae4
Jack Rimmer <jack.rimmer@and.digital>
2023-05-16 11:05:17 +0100

    Move delete to after data processing

--- src/app/main.py
+++ src/app/main.py
@@ -1,10 +1,13 @@
 import functions_framework
+from config.config_factory import ConfigFactory
 from logging_config import logging
 from services.dataset.dataset_bucket_service import DatasetBucketService
 from services.dataset.dataset_processor_service import DatasetProcessorService
 from services.validators.dataset_validator_service import DatasetValidatorService
 
 logger = logging.getLogger(__name__)
+
+config = ConfigFactory.get_config()
 
 
 @functions_framework.cloud_event
@@ -24,9 +27,9 @@ def new_dataset(cloud_event):
 
     DatasetValidatorService.validate_file_is_json(filename)
 
-    raw_dataset_with_metadata = DatasetBucketService(
-        bucket_name
-    ).get_dataset_with_cleanup(filename)
+    dataset_bucket_service = DatasetBucketService(bucket_name)
+
+    raw_dataset_with_metadata = dataset_bucket_service.get_valid_dataset(filename)
 
     logger.info("Dataset obtained from bucket successfully.")
     logger.debug(f"Dataset: {raw_dataset_with_metadata}")
@@ -34,3 +37,6 @@ def new_dataset(cloud_event):
     DatasetProcessorService().process_raw_dataset(filename, raw_dataset_with_metadata)
 
     logger.info("Dataset uploaded successfully.")
+
+    if config.AUTODELETE_DATASET_BUCKET_FILE:
+        dataset_bucket_service._try_empty_bucket()
--- src/app/services/dataset/dataset_bucket_service.py
+++ src/app/services/dataset/dataset_bucket_service.py
@@ -1,4 +1,3 @@
-from config.config_factory import ConfigFactory
 from logging_config import logging
 from models.dataset_models import UnitDataset
 from repositories.buckets.dataset_bucket_repository import DatasetBucketRepository
@@ -9,20 +8,15 @@ logger = logging.getLogger(__name__)
 
 class DatasetBucketService:
     def __init__(self, bucket_name):
-        self.config = ConfigFactory.get_config()
-
         self.bucket_name = bucket_name
         self.dataset_bucket_repository = DatasetBucketRepository(bucket_name)
 
-    def get_dataset_with_cleanup(self, filename: str) -> UnitDataset:
+    def get_valid_dataset(self, filename: str) -> UnitDataset:
         DatasetValidatorService.validate_file_is_json(filename)
 
         raw_dataset_with_metadata = (
             self.dataset_bucket_repository.get_dataset_file_as_json(filename)
         )
-
-        if self.config.AUTODELETE_DATASET_BUCKET_FILE:
-            self._try_empty_bucket()
 
         DatasetValidatorService.validate_raw_dataset(raw_dataset_with_metadata)
 
