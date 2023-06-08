b979ed51339158273c3ed7e7bfc5963761c9fc7f
Jack Rimmer <jack.rimmer@and.digital>
2023-06-08 14:32:57 +0100

    Try with setting config to none

--- src/app/repositories/firebase/firebase_loader.py
+++ src/app/repositories/firebase/firebase_loader.py
@@ -4,9 +4,9 @@ from google.cloud import firestore
 
 class FirebaseLoader:
     def __init__(self):
-        self.client = firestore.Client(project=config.PROJECT_ID)
-        self.datasets_collection = self.client.collection("datasets")
-        self.schemas_collection = self.client.collection("schemas")
+        self.client = self._connect_client()
+        self.datasets_collection = self._set_collection("datasets")
+        self.schemas_collection = self._set_collection("schemas")
 
     def get_client(self) -> firestore.Client:
         return self.client
@@ -22,6 +22,22 @@ class FirebaseLoader:
         Get the schemas collection from firestore
         """
         return self.schemas_collection
+
+    def _connect_client(self) -> firestore.Client:
+        """
+        Connect to the firestore client using PROJECT_ID
+        """
+        if config.CONF == "unit":
+            return None
+        return firestore.Client(project=config.PROJECT_ID)
+
+    def _set_collection(self, collection) -> firestore.CollectionReference:
+        """
+        Setup the collection reference for schemas and datasets
+        """
+        if config.CONF == "unit":
+            return None
+        return self.client.collection(collection)
 
 
 firebase_loader = FirebaseLoader()
