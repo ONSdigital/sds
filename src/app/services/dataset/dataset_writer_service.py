74cc4d056aba090cbc399612b39d190da6043b52
Jack Rimmer <jack.rimmer@and.digital>
2023-06-08 14:54:38 +0100

    Linting...

--- src/app/services/dataset/dataset_writer_service.py
+++ src/app/services/dataset/dataset_writer_service.py
@@ -41,7 +41,7 @@ class DatasetWriterService:
             logger.error(f"Dataset transaction error, exception raised: {e}")
             logger.error("Rolling back dataset transaction")
             raise Exception("Error performing dataset transaction.")
-        
+
         logger.info("Dataset transaction committed successfully.")
 
     def try_delete_previous_versions_datasets(
