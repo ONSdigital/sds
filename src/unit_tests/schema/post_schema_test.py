b7852a1042815f9da428930ad3264976f195fd9b
Joseph Ashcroft <joe.ashcroft@ext.ons.gov.uk>
2023-06-26 16:15:11 +0100

    fixing errors

--- src/unit_tests/schema/post_schema_test.py
+++ src/unit_tests/schema/post_schema_test.py
@@ -9,7 +9,6 @@ from repositories.firebase.schema_firebase_repository import SchemaFirebaseRepos
 from services.shared.publisher_service import PublisherService
 
 from src.test_data import schema_test_data
-from src.unit_tests.test_helper import TestHelper
 
 
 class PostSchemaTest(TestCase):
@@ -152,9 +151,7 @@ class PostSchemaTest(TestCase):
 
         SchemaFirebaseRepository.get_latest_schema_with_survey_id = MagicMock()
         SchemaFirebaseRepository.get_latest_schema_with_survey_id.return_value = (
-            TestHelper.create_document_snapshot_generator_mock(
-                [schema_test_data.test_post_schema_metadata_first_version_response]
-            )
+            schema_test_data.test_post_schema_metadata_first_version_response
         )
 
         SchemaFirebaseRepository.perform_new_schema_transaction = MagicMock()
