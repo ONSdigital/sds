032f5ad130342f35f4289b85fcb95ef87e8dc2bc
Joseph Ashcroft <joe.ashcroft@ext.ons.gov.uk>
2023-06-27 11:03:33 +0100

    linting fix

--- src/integration_tests/datasets/e2e_dataset_integration_test.py
+++ src/integration_tests/datasets/e2e_dataset_integration_test.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from unittest import TestCase
-from google.cloud import storage
 
 from config.config_factory import config
+from google.cloud import storage
 
 from src.integration_tests.helpers.integration_helpers import (
     cleanup,
@@ -45,7 +45,12 @@ class E2ESchemaIntegrationTest(TestCase):
         if create_dataset_response is not None and create_dataset_response != 200:
             assert False, "Unsuccessful request to create dataset"
 
-        assert not storage.Client().bucket(config.DATASET_BUCKET_NAME).blob(filename).exists()
+        assert (
+            not storage.Client()
+            .bucket(config.DATASET_BUCKET_NAME)
+            .blob(filename)
+            .exists()
+        )
 
         dataset_metadata_response = session.get(
             f"{config.API_URL}/v1/dataset_metadata?survey_id={dataset['survey_id']}&period_id={dataset['period_id']}",
