a3086aee1255ce9ca3f87ce4fb76d912383de893
Jack Rimmer <jack.rimmer@and.digital>
2023-07-06 09:27:07 +0100

    Add literal type hints for type clarity

--- src/app/services/shared/document_version_service.py
+++ src/app/services/shared/document_version_service.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from models.dataset_models import UnitDataset
 from models.schema_models import Schema
 
@@ -6,13 +8,14 @@ class DocumentVersionService:
     @staticmethod
     def calculate_survey_version(
         document_current_version: Schema | UnitDataset,
-        version_key: str,
+        version_key: Literal["sds_dataset_version", "sds_schema_version"],
     ) -> int:
         """
         Calculates the next version number of a document based on a version key, returning 1 by default if no document exists.
 
         Parameters:
-        version_key (str): the key being accessed to find out the document version.
+        document_current_version: document that the version is being calculated form
+        version_key: the key being accessed to find out the document version.
         """
         if document_current_version is None:
             return 1
