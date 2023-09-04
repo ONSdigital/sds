ba18c54351ee69bbcfca19ab91e06c6311d60f65
Mohammed1804 <101636555+Mohammed1804@users.noreply.github.com>
2023-09-04 11:51:56 +0100

    changed webhook link

--- src/app/exception/exception_interceptor.py
+++ src/app/exception/exception_interceptor.py
@@ -1,7 +1,7 @@
 from exception.exception_responder import ExceptionResponder
 from fastapi import Request, status
 from fastapi.responses import JSONResponse
-
+import Slack_posts
 
 class ExceptionInterceptor:
     def throw_500_global_exception(request: Request, exc: Exception) -> JSONResponse:
@@ -11,6 +11,7 @@ class ExceptionInterceptor:
         er = ExceptionResponder(
             status.HTTP_500_INTERNAL_SERVER_ERROR, "error", "Unable to process request"
         )
+        Slack_posts.send_slack_notification("Error, Unable to proceess request in the application")
         return er.throw_er_with_json()
 
     def throw_400_validation_exception(
@@ -22,6 +23,7 @@ class ExceptionInterceptor:
         er = ExceptionResponder(
             status.HTTP_400_BAD_REQUEST, "error", "Validation has failed"
         )
+        Slack_posts.send_slack_notification("Error, A validation error has occured in the application")
         return er.throw_er_with_json()
 
     def throw_400_incorrect_schema_key_exception(
