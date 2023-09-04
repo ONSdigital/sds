b1bb2bee095d24de6654b30f7d25197897ba33c0
Mohammed1804 <101636555+Mohammed1804@users.noreply.github.com>
2023-09-04 11:51:56 +0100

    changed webhook link

---
+++ Slack_posts.py
@@ -0,0 +1,13 @@
+import requests
+
+
+def send_slack_notification(message):
+    webhook_url = "https://hooks.slack.com/services/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"
+    payload = {"text": message}
+    response = requests.post(webhook_url, json=payload)
+    if response.status_code == 200:
+        print("Notification sent successfully.")
+    else:
+        print(
+            f"Failed to send notification. Status code: {response.status_code}, Response: {response.text}"
+        )
