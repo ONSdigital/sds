29697f0d8b39767b0c390e1aa8d6d7dcf6e34c07
Jonny Adcock <jonathan.adcock@and.digital>
2023-12-05 11:12:57 +0000

    updating env vars in deployment script

--- scripts/deploy_to_dev_project.sh
+++ scripts/deploy_to_dev_project.sh
@@ -2,12 +2,20 @@
 
 # Unset all relevant envars
 echo "Unsetting all relevant env variables..."
+unset API_URL
+unset AUTODELETE_DATASET_BUCKET_FILE
 unset BUILD_ID
-unset CI_STORAGE_BUCKET_NAME
-unset DEFAULT_HOSTNAME
-unset GOOGLE_APPLICATION_CREDENTIALS
+unset DATASET_BUCKET_NAME
+unset FIRESTORE_DB_NAME
+unset LOG_LEVEL
 unset OAUTH_CLIENT_ID
+unset OAUTH_CLIENT_NAME
 unset PROJECT_ID
+unset PUBLISH_DATASET_TOPIC_ID
+unset PUBLISH_SCHEMA_TOPIC_ID
+unset RETAIN_DATASET_FIRESTORE
+unset SCHEMA_BUCKET_NAME
+unset SURVEY_MAP_URL
 
 # Prompt the user for their GCP project ID and store it in a variable
 read PROJECT_ID"?Enter your GCP project ID: "
@@ -16,7 +24,7 @@ SSL_CERT_NAME="${PROJECT_ID}-sds-ssl-cert"
 
 AUTODELETE_DATASET_BUCKET_FILE="True"
 CI_STORAGE_BUCKET_NAME=$PROJECT_ID
-DATASET_BUCKET_NAME=ons-sds-preprod-sds-europe-west2-dataset
+DATASET_BUCKET_NAME=${PROJECT_ID}-sds-europe-west2-dataset
 DEFAULT_HOSTNAME=$(gcloud compute ssl-certificates describe $SSL_CERT_NAME \
     --format='value(subjectAlternativeNames)')
 FIRESTORE_DB_NAME=${PROJECT_ID}-sds
@@ -25,9 +33,11 @@ PUBLISH_DATASET_TOPIC_ID="ons-sds-publish-dataset"
 PUBLISH_SCHEMA_TOPIC_ID="ons-sds-publish-schema"
 REGION="europe-west2"
 RETAIN_DATASET_FIRESTORE="True"
-SCHEMA_BUCKET_NAME=ons-sds-preprod-sds-europe-west2-schema
+SCHEMA_BUCKET_NAME=o${PROJECT_ID}-sds-europe-west2-schema
 SURVEY_MAP_URL=https://raw.githubusercontent.com/ONSdigital/sds-schema-definitions/main/mapping/survey_map.json
 URL_SCHEME="https"
+
+API_URL="${URL_SCHEME}://${DEFAULT_HOSTNAME}"
 
 echo "Setting project to $PROJECT_ID..."
 gcloud config set project $PROJECT_ID
@@ -65,22 +75,36 @@ OAUTH_CLIENT_NAME=$(gcloud iap oauth-clients list ${OAUTH_BRAND_NAME} --format='
     --limit=1)
 
 echo "Setting env variables for $PROJECT_ID project..."
-export API_URL=
+export API_URL=${API_URL}
+echo "API_URL=${API_URL}"
+export AUTODELETE_DATASET_BUCKET_FILE=${AUTODELETE_DATASET_BUCKET_FILE}
+echo "AUTODELETE_DATASET_BUCKET_FILE=${AUTODELETE_DATASET_BUCKET_FILE}"
 export BUILD_ID=$BUILD_ID
 echo "BUILD_ID: ${BUILD_ID}"
-export CI_STORAGE_BUCKET_NAME=$CI_STORAGE_BUCKET_NAME
-echo "CI_STORAGE_BUCKET_NAME: ${CI_STORAGE_BUCKET_NAME}"
-export DEFAULT_HOSTNAME=$(gcloud compute ssl-certificates describe $SSL_CERT_NAME \
-    --format='value(subjectAlternativeNames)')
-echo "DEFAULT_HOSTNAME: ${DEFAULT_HOSTNAME}"
 echo "GOOGLE_APPLICATION_CREDENTIALS: ${GOOGLE_APPLICATION_CREDENTIALS}"
+export DATASET_BUCKET_NAME=${DATASET_BUCKET_NAME}
+echo "DATASET_BUCKET_NAME=${DATASET_BUCKET_NAME}"
+export FIRESTORE_DB_NAME=${FIRESTORE_DB_NAME}
+echo "FIRESTORE_DB_NAME=${FIRESTORE_DB_NAME}"
+export LOG_LEVEL=${LOG_LEVEL}
+echo "LOG_LEVEL=${LOG_LEVEL}"
 # gcloud returns client name as '$OAUTH_BRAND_NAME/identityAwareProxy/OAUTH_CLIENT_ID' so we have to
 # split by / and use the last part of the string here to get client id
 export OAUTH_CLIENT_ID=${${OAUTH_CLIENT_NAME}##*/}
-echo "OAUTH_CLIENT_ID: ${OAUTH_CLIENT_ID}"
-export PROJECT_ID=$PROJECT_ID
-echo "PROJECT_ID: ${PROJECT_ID}"
-export URL_SCHEME="https"
-echo "URL_SCHEME: ${URL_SCHEME}"
+echo "OAUTH_CLIENT_ID=${OAUTH_CLIENT_ID}"
+export OAUTH_CLIENT_NAME=${OAUTH_CLIENT_NAME}
+echo "OAUTH_CLIENT_NAME=${OAUTH_CLIENT_NAME}"
+export PROJECT_ID=${PROJECT_ID}
+echo "PROJECT_ID=${PROJECT_ID}"
+export PUBLISH_DATASET_TOPIC_ID=${PUBLISH_DATASET_TOPIC_ID}
+echo "PUBLISH_DATASET_TOPIC_ID=${PUBLISH_DATASET_TOPIC_ID}"
+export PUBLISH_SCHEMA_TOPIC_ID=${PUBLISH_SCHEMA_TOPIC_ID}
+echo "PUBLISH_SCHEMA_TOPIC_ID=${PUBLISH_SCHEMA_TOPIC_ID}"
+export RETAIN_DATASET_FIRESTORE=${RETAIN_DATASET_FIRESTORE}
+echo "RETAIN_DATASET_FIRESTORE=${RETAIN_DATASET_FIRESTORE}"
+export SCHEMA_BUCKET_NAME=${SCHEMA_BUCKET_NAME}
+echo "SCHEMA_BUCKET_NAME=${SCHEMA_BUCKET_NAME}"
+export SURVEY_MAP_URL=${SURVEY_MAP_URL}
+echo "SURVEY_MAP_URL=${SURVEY_MAP_URL}"
 
 echo "Done!"
