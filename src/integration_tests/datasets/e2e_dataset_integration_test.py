4b1f69c44b9cec5ec597ab00210d8f5f9c623af9
jamesbryer <james.bryer@ext.ons.gov.uk>
2024-05-23 14:56:57 +0100

    Add delay to try to limit 404 messages

--- src/integration_tests/datasets/e2e_dataset_integration_test.py
+++ src/integration_tests/datasets/e2e_dataset_integration_test.py
@@ -1,3 +1,4 @@
+import time
 from unittest import TestCase
 
 from config.config_factory import config
@@ -30,6 +31,7 @@ from src.test_data.shared_test_data import (
 class E2EDatasetIntegrationTest(TestCase):
     def setUp(self) -> None:
         cleanup()
+        time.sleep(5)
         pubsub_setup(dataset_pubsub_helper, test_dataset_subscriber_id)
         pubsub_setup(dataset_error_pubsub_helper, test_dataset_error_subscriber_id)
 
--- src/integration_tests/schemas/e2e_schema_integration_test.py
+++ src/integration_tests/schemas/e2e_schema_integration_test.py
@@ -1,3 +1,4 @@
+import time
 from unittest import TestCase
 
 from src.app.config.config_factory import config
@@ -17,6 +18,7 @@ from src.test_data.shared_test_data import test_schema_subscriber_id
 class E2ESchemaIntegrationTest(TestCase):
     def setUp(self) -> None:
         cleanup()
+        time.sleep(5)
         pubsub_setup(schema_pubsub_helper, test_schema_subscriber_id)
 
     def tearDown(self) -> None:
