40be272698b2b6ca661932f17f5b454ffde182d2
oms09 <omar.moulana@ons.gov.uk>
2024-07-25 15:08:15 +0100

    test

--- src/unit_tests/schema/get_schema_metadata_collection_test.py
+++ src/unit_tests/schema/get_schema_metadata_collection_test.py
@@ -7,12 +7,10 @@ from src.test_data import schema_test_data
 
 def test_get_schema_metadata_collection_200_response(test_client):
     """
-    When the schema is retrieved successfully from the bucket there should be a 200 status code and expected response.
+    When the schema is retrieved successfully from the there should be a 200 status code and expected response.
     """
     SchemaFirebaseRepository.get_schema_metadata_collection = MagicMock()
-    SchemaFirebaseRepository.get_schema_metadata_collection.return_value = (
-        schema_test_data.test_schema_metadata_collection
-    )
+
 
     response = test_client.get("/v1/schema_metadata?survey_id=test_survey_id")
 
