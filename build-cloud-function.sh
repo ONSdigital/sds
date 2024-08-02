98a94a06695f777e1da24e4d05bf78109d0ee1c4
jamesbryer <james.bryer@ext.ons.gov.uk>
2024-08-02 09:24:56 +0100

    Update comments

--- build-cloud-function.sh
+++ build-cloud-function.sh
@@ -1,21 +1,17 @@
 # Install jq for JSON parsing
 apt-get -y update && apt-get install -y jq
 
-# Get _IAC_BRANCH from argument, if empty string, set to 'main'
+# Get values from arguments
 _IAC_BRANCH=${1:-main}
-
-# Get SDS branch from argument
 _SDS_BRANCH=${2}
 
 
-# Trigger the build and store the response - dump the stdout to /dev/null to avoid cluttering the logs with the JSON response
+# Trigger the build and store the response as a file because gcloud won't assign the output to a variable for some reason
 echo "Triggering build on IaC branch: ${_IAC_BRANCH}..."
 gcloud builds triggers run update-new-dataset-cloud-function --region=europe-west2 --branch=$_IAC_BRANCH --substitutions=_SDS_BRANCH=$_SDS_BRANCH --format=json | tee trigger_response.json >/dev/null
 
 # Extract the build ID from the trigger response
 _BUILD_ID=$(cat trigger_response.json | jq -r '.metadata.build.id')
-
-# remove the trigger response file
 rm trigger_response.json
 
 echo "Triggered build with ID: ${_BUILD_ID}"
@@ -24,7 +20,6 @@ echo "Triggered build with ID: ${_BUILD_ID}"
 while true; do
     gcloud builds describe $_BUILD_ID --format=json --region=europe-west2 | tee build_status.json > /dev/null
     _BUILD_STATUS=$(cat build_status.json | jq -r '.status')
-    # remove the build status file
     rm build_status.json
     echo "Current build status: ${_BUILD_STATUS}"
     if [[ "${_BUILD_STATUS}" == "SUCCESS" ]]; then
