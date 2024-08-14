f19d9b5cf334345862fc482e9ffc6b1e793a38a9
jamesbryer <james.bryer@ext.ons.gov.uk>
2024-08-14 11:00:09 +0100

    Update to retrieve oldest dataset file from bucket

--- src/app/repositories/buckets/dataset_bucket_repository.py
+++ src/app/repositories/buckets/dataset_bucket_repository.py
@@ -22,16 +22,22 @@ class DatasetBucketRepository(BucketRepository):
         """
         return self.get_bucket_file_as_json(filename)
 
-    def fetch_first_filename_from_bucket(self) -> str | None:
-        """
-        Fetches the first filename from the bucket.
+    def fetch_oldest_filename_from_bucket(self) -> str | None:
+            """
+            Fetches the filename with the oldest 'last modified' date from the bucket.
 
-        Returns:
-        str: filename from the bucket.
-        """
-        blobs = self.bucket.list_blobs()
+            Returns:
+            str: filename with the oldest 'last modified' date from the bucket.
+            """
+            blobs = self.bucket.list_blobs()
+            oldest_blob = None
+            oldest_date = None
 
-        for blob in blobs:
-            return blob.name
+            for blob in blobs:
+                last_modified = blob.updated
 
-        return None
+                if oldest_date is None or last_modified < oldest_date:
+                    oldest_date = last_modified
+                    oldest_blob = blob
+
+            return oldest_blob.name if oldest_blob else None
