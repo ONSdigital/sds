bf9d8c0f2c7274fa04c65167a3ca2e017aa1a6a6
“JacobHarrisonONS” <jacobharrison@outlook.com>
2024-08-21 11:32:29 +0100

    comment out create topic

--- src/app/services/shared/publisher_service.py
+++ src/app/services/shared/publisher_service.py
@@ -26,7 +26,6 @@ class PublisherService:
         publish_data: data to be sent to the pubsub topic,
         topic_id: unique identifier of the topic the data is published to
         """
-        self.create_topic(topic_id)
         topic_path = self.publisher.topic_path(config.PROJECT_ID, topic_id)
         self._verify_topic_exists(topic_path)
 
@@ -43,12 +42,12 @@ class PublisherService:
         except Exception:
             raise exceptions.ExceptionTopicNotFound
 
-    def create_topic(self, topic_id) -> None:
-        topic_path = self.publisher.topic_path(config.PROJECT_ID, topic_id)
-        """Create a new Pub/Sub topic."""
-        logger.debug("create_topic")
-        topic = self.publisher.create_topic(request={"name": topic_path})
-        logger.debug(f"Created topic: {topic.name}")
+    # def create_topic(self, topic_id) -> None:
+    #     topic_path = self.publisher.topic_path(config.PROJECT_ID, topic_id)
+    #     """Create a new Pub/Sub topic."""
+    #     logger.debug("create_topic")
+    #     topic = self.publisher.create_topic(request={"name": topic_path})
+    #     logger.debug(f"Created topic: {topic.name}")
 
 
 publisher_service = PublisherService()
