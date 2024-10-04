729570f3157a06cd6fadf507a4e419b6f121cc62
oms09 <omar.moulana@ons.gov.uk>
2024-10-04 13:00:14 +0100

    removed same test

--- src/unit_tests/service/test_byte_conversion_service.py
+++ src/unit_tests/service/test_byte_conversion_service.py
@@ -11,11 +11,6 @@ class TestByteConversionService:
         expected_size = 5
         assert expected_size == ByteConversionService.get_serialized_size(input) 
 
-        
-        input = {"key": "cat"}
-        expected_size = 14
-        assert expected_size == ByteConversionService.get_serialized_size(input) 
-
     def test_get_serialized_size_empty(self):
         input = {}
         expected_size = 2
