255413a77dcd37435d68766eaa7e299803046ef8
oms09 <omar.moulana@ons.gov.uk>
2024-10-10 13:22:55 +0100

    Delete constant

--- src/app/repositories/firebase/dataset_firebase_repository.py
+++ src/app/repositories/firebase/dataset_firebase_repository.py
@@ -10,7 +10,6 @@ logger = logging.getLogger(__name__)
 
 class DatasetFirebaseRepository:
     MAX_BATCH_SIZE_BYTES = 9 * 1024 * 1024
-    DELETE_BATCH_SIZE = 100
 
     def __init__(self):
         self.client = firebase_loader.get_client()
