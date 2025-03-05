68ac4915ea67803ae182645387d30135c4f0264d
jamesbryer <james.bryer@ext.ons.gov.uk>
2025-03-05 12:14:38 +0000

    Remove joint endpoint.

--- src/app/models/joint_models.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from src.app.models.dataset_models import DatasetMetadata
-from src.app.models.schema_models import SchemaMetadata
-
-
-@dataclass
-class JointMetadata:
-    datasets: list[DatasetMetadata]
-    schemas: list[SchemaMetadata]
