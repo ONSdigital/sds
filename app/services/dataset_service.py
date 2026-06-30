b7f05d4560948195cffbfc81ba6203e3dfa70e47
Angus <Angus.goody@ons.gov.uk>
2026-06-30 14:30:49 +0100

    added dataset service stubs

--- app/services/dataset_service.py
+++ app/services/dataset_service.py
@@ -1,4 +1,56 @@
+from app.models.collection_exericise_end_data import CollectionExerciseEndData
+from app.models.dataset_models import DatasetMetadata, UnitDataset
 
 
 class DatasetService:
-    pass
+
+    def __init__(self):
+        pass
+
+    def get_dataset_metadata_collection(
+            self,
+            survey_id: str,
+            period_id: str
+    ) -> list[DatasetMetadata]:
+        """
+        Gets the collection of dataset metadata associated for
+        a specific survey and period id.
+
+        :param survey_id: survey id of the collection.
+        :param period_id: period id of the collection.
+        """
+        pass
+
+    def get_all_dataset_metadata_collection(
+            self
+    ) -> list[DatasetMetadata]:
+        """
+        Gets the collection of dataset metadata
+        for ALL surveys held in SDS.
+        """
+        pass
+
+    def get_unit_supplementary_data(
+            self,
+            dataset_id: str,
+            identifier: str
+    ) -> UnitDataset | None:
+        """
+        Retrieve supplementary data for a particular unit given the dataset id and identifier.
+
+        :param dataset_id: The dataset id.
+        :param identifier: The unit identifier.
+        """
+        pass
+
+    def end_collection_exercise(
+            self,
+            collection_exercise_end_data: CollectionExerciseEndData
+    ):
+        """
+        When a collection exercise ends, the message is received
+        and the dataset is marked for deletion
+
+        :param collection_exercise_end_data: The collection exercise end data.
+        """
+        pass
