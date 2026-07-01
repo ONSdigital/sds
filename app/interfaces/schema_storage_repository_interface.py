c395fe5d62c1ca1b9006e53ddb4f3a4932224d4c
Angus <Angus.goody@ons.gov.uk>
2026-07-01 12:32:38 +0100

    schema interface

---
+++ app/interfaces/schema_storage_repository_interface.py
@@ -0,0 +1,69 @@
+from abc import ABC, abstractmethod
+
+from app.models.schema_models import SchemaMetadata
+
+
+class SchemaStorageRepositoryInterface(ABC):
+    """
+    This interface defines where schemas are
+    stored
+    """
+
+    @abstractmethod
+    def get_latest_schema_metadata(
+            self,
+            survey_id: str
+    ) -> SchemaMetadata | None:
+        """
+        Get the latest schema metadata for a specific survey
+
+        :param survey_id: The id of the dataset survey
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_latest_guid(
+            self,
+            survey_id: str,
+    ) -> str | None:
+        """
+        Get the guid of a schema with a specific survey and latest version
+
+        :param survey_id: The survey id of the survey being queried.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_guid(
+            self,
+            survey_id: str,
+            version: str
+    ) -> str | None:
+        """
+        Get the guid of a schema with a specific survey id and version
+
+        :param survey_id: The survey id of the survey being queried.
+        :param version: The version of the survey being queried
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_metadata(
+            self,
+            survey_id: str
+    ) -> list[SchemaMetadata]:
+        """
+        Gets the collection of schema metadata with a specific survey id.
+
+        :param survey_id: The survey id of the survey being queried.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_all_metadata(
+            self,
+    ) -> list[SchemaMetadata]:
+        """
+        Gets the collection of schema metadata for all surveys.
+        """
+        raise NotImplementedError
